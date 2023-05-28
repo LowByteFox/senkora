ae0b994bb9bf3eae2e352d01a27d1cd9e2af0bb4
Fire-The-Fox <gajdos.jan77@gmail.com>
2023-05-28 15:39:59 +0200

    print func test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
+#include "v8-data.h"
+#include "v8-maybe.h"
+#include "v8-promise.h"
+#include "v8-script.h"
 #include <v8.h>
+#include <v8-isolate.h>
 #include <libplatform/libplatform.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+const char* ToCString(const v8::String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
+}
+
+void Print(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  bool first = true;
+  for (int i = 0; i < args.Length(); i++) {
+    v8::HandleScope handle_scope(args.GetIsolate());
+    if (first) {
+      first = false;
+    } else {
+      printf(" ");
+    }
+    v8::String::Utf8Value str(args.GetIsolate(), args[i]);
+    const char* cstr = ToCString(str);
+    printf("%s", cstr);
+  }
+  printf("\n");
+  fflush(stdout);
+}
 
 int main(int argc, char* argv[]) {
   // Initialize V8.
@@ -12,22 +38,26 @@ int main(int argc, char* argv[]) {
   std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
   v8::V8::InitializePlatform(platform.get());
   v8::V8::Initialize();
+
   // Create a new Isolate and make it the current one.
   v8::Isolate::CreateParams create_params;
   create_params.array_buffer_allocator =
       v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+
   v8::Isolate* isolate = v8::Isolate::New(create_params);
   {
     v8::Isolate::Scope isolate_scope(isolate);
     // Create a stack-allocated handle scope.
     v8::HandleScope handle_scope(isolate);
+    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
+    global->Set(isolate, "print", v8::FunctionTemplate::New(isolate, Print));
     // Create a new context.
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
+    v8::Local<v8::Context> context = v8::Context::New(isolate, nullptr, global);
     // Enter the context for compiling and running the hello world script.
     v8::Context::Scope context_scope(context);
     // Create a string containing the JavaScript source code.
     v8::Local<v8::String> source =
-        v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'",
+        v8::String::NewFromUtf8(isolate, "print({ h: 'b' })",
                                 v8::NewStringType::kNormal)
             .ToLocalChecked();
     // Compile the source code.
@@ -35,11 +65,9 @@ int main(int argc, char* argv[]) {
         v8::Script::Compile(context, source).ToLocalChecked();
     // Run the script to get the result.
     v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-    // Convert the result to an UTF8 string and print it.
-    v8::String::Utf8Value utf8(isolate, result);
-    printf("%s\n", *utf8);
   }
   // Dispose the isolate and tear down V8.
+
   isolate->Dispose();
   v8::V8::Dispose();
   v8::V8::ShutdownPlatform();
