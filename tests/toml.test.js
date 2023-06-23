cd420a104a2b0ce674c57797c0be9e5d94d1f636
Tiramify <tiramify@gmail.com>
2023-06-23 23:06:27 +0200

    Impl stringifyForOutput Fix compArrays/compObject

---
+++ tests/toml.test.js
@@ -0,0 +1,22 @@
+import { expect, describe, test } from "senkora:test";
+import { readFromFile } from "senkora:fs";
+import { parse } from "senkora:toml";
+
+const content = readFromFile("test.toml");
+
+describe("toml", () => {
+    test("parse", () => {
+        const parsed = parse(content);
+
+        expect(parsed).toEqual({
+            "ahoj": "cau",
+            "config": {
+                "daco": [{ "a": 1, "b": 2 }, { "a": 3, "b": 4 }],
+                "nested": {
+                    "something": [[1, 2], [3, 4]]
+                },
+                "something": "ahoj"
+            }
+        });
+    })
+});
